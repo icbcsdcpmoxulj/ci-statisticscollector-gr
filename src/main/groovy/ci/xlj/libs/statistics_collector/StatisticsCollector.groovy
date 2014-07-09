e84e735840a20574a73d21cc46d2afc04dde57ed
kfzx-xulj <kfzx-xulj@KFZXXMPUB012>
2014-07-09 15:53:25 +0800

    v1.0.0

--- src/main/groovy/ci/xlj/libs/statistics_collector/StatisticsCollector.groovy
+++ src/main/groovy/ci/xlj/libs/statistics_collector/StatisticsCollector.groovy
@@ -1,35 +1,36 @@
 package ci.xlj.libs.statistics_collector
 
+import java.util.logging.Logger
+
 import org.apache.http.client.methods.HttpPost
 import org.apache.http.entity.StringEntity
 import org.apache.http.impl.client.DefaultHttpClient
 import org.apache.http.util.EntityUtils
-import org.apache.log4j.Logger
 
 import ci.xlj.libs.utils.StringUtils
 
 class StatisticsCollector {
 
-	private Logger logger = Logger.getLogger(StatisticsCollector)
+	private static Logger logger = Logger.getLogger("ci.xlj.libs.statistics_collector.StatisticsCollector")
 
 	private StatisticsCollector(){
 	}
 
-	private DefaultHttpClient client
-	def serverUrl
+	private static client
+	def static serverUrl
 
-	def StatisticsCollector getInstance(serverURL){
-		this.serverUrl = StringUtils.transformToUrl(serverURL)
-		logger.info("Server URL is '$serverUrl'.")
+	def static init(serverURL){
+		this.serverUrl = serverURL
 
 		client = new DefaultHttpClient()
 	}
 
-	def post
-	def entity
-	def response
-	
-	def sendMessage(msg){
+	def static post
+	def static entity
+	def static response
+	def static responseContent
+
+	def static sendMessage(msg){
 		post = new HttpPost(serverUrl)
 		post.setHeader("Content-Type", "text/xml;charset=UTF-8")
 
@@ -40,7 +41,30 @@ class StatisticsCollector {
 
 		response = client.execute(post)
 		entity = response.getEntity()
-
+		responseContent=getHtmlFromHttpEntity()
+		def retCode=response.getStatusLine().getStatusCode()
+		if(retCode!=200){
+			logger.info("Send OK.")
+		}else{
+			logger.info("Send Result: ${retCode} ${responseContent}")
+		}
+		
 		EntityUtils.consume(entity)
+	}
+
+	private static getHtmlFromHttpEntity() {
+		def htmlContent = ""
+
+		try {
+			def reader = new BufferedReader(new InputStreamReader(
+					entity.getContent(), "utf-8"))
+
+			reader.eachLine { htmlContent<<=it }
+		} catch (Exception e) {
+			logger.info("Error in transforming HTTP Entity into String. Details:\n"
+					+ StringUtils.getStrackTrace(e))
+		}
+
+		return htmlContent.toString()
 	}
 }
